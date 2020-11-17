3f0b10feb535a072344278880f0d60fd5cd25648
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-11-17 14:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 main.remove();
-let newHeader=createElement('h1');
+/*let newHeader=createElement('h1');
 newHeader.setAttribute('id', 'victory');
 newHeader.innerHTML='Jo is the champion';
-document.querySelector("body").appendChild("newHeader");
+document.querySelector("body").appendChild("newHeader");*/
+
+document.querySelector('body').innerHTML="<h1 id='victory'>Jo is the champion</h1>";
